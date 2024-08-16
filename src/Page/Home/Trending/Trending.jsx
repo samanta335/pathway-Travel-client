adbb772b342376b05b4786f6668b8495adabfb0c
samanta <121935945+samanta335@users.noreply.github.com>
2024-08-16 14:04:22 +0600

    responsive done

--- src/Page/Home/Trending/Trending.jsx
+++ src/Page/Home/Trending/Trending.jsx
@@ -8,33 +8,33 @@ const Trending = () => {
 <h1 className='text-4xl text-center font-bold '>Our Trending Tour</h1>
 <hr className='w-4/5 lg:w-1/2 mx-auto my-8'/>
 <div className=''>
-<div className='flex justify-center items-center '>
-            <div className="rounded-full w-[112px] h-[112px] md:w-72 md:h-72 lg:w-96 lg:h-96 xl:w-60 xl:h-60 ">
+<div className='flex-cols  lg:flex justify-center items-center'>
+            <div className="rounded-full mx-auto lg:mx-0 w-[250px] h-[250px] md:w-72 md:h-72 lg:w-96 lg:h-96 xl:w-60 xl:h-60 ">
                                     <img
                                         src="https://i.postimg.cc/J43pcPC1/saint-martin.jpg"
                                         alt="Sajek"
                                         className="object-cover w-full h-full transition-transform  transform hover:scale-105 rounded-full "
                                         style={{ border: '4px solid blue' }}
                                     />
-<p className="text-center mt-2  text-lg lg:text-xl font-bold ">Saint Martin</p>
+<p className="text-center mt-2  text-xl font-bold ">Saint Martin</p>
   </div>
-  <div className="rounded-full w-[112px] h-[112px] md:w-72 md:h-72 lg:w-96 lg:h-96 xl:w-60 xl:h-60 mx-4 lg:mx-16 ">
+  <div className="rounded-full mx-auto w-[250px] h-[250px] md:w-72 md:h-72 lg:w-96 lg:h-96 xl:w-60 xl:h-60  lg:mx-14 my-14 lg:my-0">
                                     <img
                                         src={cox}
                                         alt="Sajek"
                                         className="object-cover w-full h-full transition-transform  transform hover:scale-105 rounded-full"
                                         style={{ border: '4px solid blue' }}
                                     />
-<p className="text-center mt-2  text-lg lg:text-xl font-bold ">Marin Drive</p>
+<p className="text-center mt-2  text-xl font-bold ">Marin Drive</p>
  </div>
-            <div className="rounded-full w-[112px] h-[112px] md:w-72 md:h-72 lg:w-96 lg:h-96 xl:w-60 xl:h-60  relative">
+            <div className="rounded-full mx-auto  lg:mx-0 w-[250px] h-[250px] md:w-72 md:h-72 lg:w-96 lg:h-96 xl:w-60 xl:h-60  relative">
                                     <img
                                         src="https://as2.ftcdn.net/v2/jpg/06/99/45/61/1000_F_699456135_myZiVNvI1UaEZ24BByzxmpazcLU22lZ0.jpg"
                                         alt=""
                                         className="object-cover w-full h-full transition-transform  transform hover:scale-105 rounded-full"
                                         style={{ border: '4px solid blue' }}
                                     />
-<p className="text-center mt-2 text-lg lg:text-xl font-bold ">Jaflong</p>
+<p className="text-center mt-2 text-xl font-bold ">Jaflong</p>
   </div>
   </div>
 </div>
